e22ba67d58ade0abe2a60526eea92fc714bc4c0a
alan Dittler <dittler.a@gmail.com>
2020-09-22 22:47:06 -0300

    update the navbar with a modal all made in vanilla JS

--- scripts/UIstyle.js
+++ scripts/UIstyle.js
@@ -1,3 +1,4 @@
+// burger and nav code
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-link");
 const links = document.querySelectorAll(".nav-link li");
@@ -7,4 +8,40 @@ hamburger.addEventListener("click", () => {
   links.forEach((link) => {
     link.classList.toggle("fade");
   });
+});
+
+// modal triggers
+const closeBTN = document.querySelector(".closeBTN");
+const modal = document.querySelector(".modal");
+const accountInfoBTN = document.getElementById("accountInfoBTN");
+const logInBTN = document.getElementById("loginBTN");
+const logOutBTN = document.getElementById("logoutBTN");
+
+// account info modal
+accountInfoBTN.addEventListener("click", (e) => {
+  e.preventDefault();
+  modal.style.display = "block";
+});
+
+// login modal
+logInBTN.addEventListener("click", (e) => {
+  e.preventDefault();
+  modal.style.display = "block";
+});
+
+// logout modal
+logOutBTN.addEventListener("click", (e) => {
+  e.preventDefault();
+  modal.style.display = "block";
+});
+
+closeBTN.addEventListener("click", (e) => {
+  e.preventDefault();
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
 });
